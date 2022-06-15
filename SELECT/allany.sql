8a8b0242e49444601aa0bb6ccd1cad243ed5cc0f
namyeol <skaduf73@gmail.com>
2022-06-15 12:41:19 +0900

    all any

--- SELECT/allany.sql
+++ SELECT/allany.sql
@@ -1,5 +1,6 @@
+-- all : 서브쿼리 값을 포함시키지 않음
 select eno, ename, job, salary from employee where salary <= all (
     select salary from employee where job='SALESMAN');
-
+-- any : 서브쿼리 값을 포함 시킴
 select eno, ename, job, salary from employee where salary < any (
     select salary from employee where job='SALESMAN');
