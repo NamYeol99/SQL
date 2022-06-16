c96b201e7f26e13efc59106be3370fee04384715
namyeol <skaduf73@gmail.com>
2022-06-16 09:06:47 +0900

    연습문제

--- 연습문제/DDL_DML.sql
+++ 연습문제/DDL_DML.sql
@@ -1,1 +1,69 @@
-select * from employee;
+-- 1. 다음표에 명시된 대로 dtpt2 테이블을 생성하시오.
+create table dept2(dno number(2,0), dname varchar2(14), loc varchar2(13), regdate timestamp);
+
+-- 2. 다음표에 명시된 대로 emp2 테이블을 생성하시오.
+create table emp2(eno number(2,0), ename varchar2(14), dno number(2,0), regdate timestamp);
+
+-- 3. 위에서 작성한 emp2 테이블을 아래와 같이 수정하시오.
+desc emp2;
+alter table emp2 modify ENO number(4);
+alter table emp2 modify ENAME varchar2(25);
+alter table emp2 modify regdate date;
+
+-- 4. employee 테이블을 복사해서 employee2 테이블을 생성하되 컬럼명을 (emp_id, name, sal, dept_id)으로만 지정하시오.
+drop table employee2;
+create table employee2(emp_id, name, sal, dept_id) as select eno, ename, salary, dno from employee;
+desc employee2;
+
+-- 5. employee2 테이블을 emp3로 변경하시오.
+alter table employee2 rename to emp3;
+desc emp3;
+
+-- 6. emp2와 emp3 테이블을 지우기 위한 쿼리를 작성하시오.
+drop table emp2;
+drop table emp3;
+
+-- 7. dept2 테이블에서 dname을 지우시오.
+desc dept2;
+alter table dept2 drop column dname;
+
+-- 8. dept2 테이블에서 loc 칼럼을 unused로 표시하시오.
+alter table dept2 set unused(loc);
+
+-- 9. dept 테이블에서 unused 칼럼을 모두 제거하시오
+alter table dept2 drop unused columns;
+
+-- 10. employee 테이블의 구조만 복사하여 emp_insert란 이름의 빈 테이블을 만드시오.
+create table emp_insert as SELECT * FROM employee where 1=0;
+
+-- 11. 본인을 emp_insert 테이블에 추가하되 sysdate를 이용해서 입사일을 오늘 날짜로 입력하시오. 
+insert into emp_insert (ename,hiredate) values('kny', sysdate);
+select * from emp_insert;
+
+-- 12. emp_insert 테이블에 옆 사람을 추가하되 to_date 함수를 이용해서 입사일을 어제 날짜로 입력하시오.
+insert into emp_insert (ename,hiredate) values('aaa', to_date('22/06/14','yy/mm/dd'));
+
+-- 13. employee 테이블의 구조와 내용을 복사하여 emp_copy 테이블을 만드시오.
+create table emp_copy as select * from employee;
+
+-- 14. emp_copy 테이블에서 사원번호가 7788인 사원의 부서번호를 10번으로 수정하시오.
+select * from emp_copy;
+update emp_copy set dno=10 where eno = 7788;
+-- 15. emp_copy 테이블에서 사원번호가 7788인 사원의 담당업무 및 급여를 사원번호 7499의 담당업무와 급여를 일치시키시오.
+update emp_copy set(job,salary) = (
+    select job,salary from emp_copy where eno = 7499) where eno = 7788;
+    
+-- 16. emp_copy 테이블에서 사원번호가 7369의 업무가 동일한 모든 사원의 부서번호를 7369사원의 부서번호로 갱신하시오.
+update emp_copy set dno =(
+    select dno from emp_copy where eno = 7369) where job=(
+    select job from emp_copy where eno = 7369);
+    
+-- 17. department 테이블의 구조와 내용을 복사하여 dept_copy란 이름의 테이블로 만드시오.
+create table dept_copy as select * from department;
+
+-- 18. dept_copy 테이블에서 부서명이 research인 부서를 제거하시오.
+select * from dept_copy;
+delete dept_copy where dname = 'RESEARCH';
+
+-- 19. dept_copy 테이블에서 부서번호가 10, 40인 부서를 제거하시오
+delete dept_copy where dno in(10,40);
--- 연습문제/select2.sql
+++ 연습문제/select2.sql
@@ -1,13 +1,14 @@
-select avg(salary) from employee;
+select dno, avg(salary) from employee group by dno;
 -- 1. 각 부서별 평균 급여에 대하여 -500 +500 사이의 범위에 있는 사원의 부서번호, 사원버호, 이름, 급여를 구하시오.
-select ename, salary ,dno from employee where salary > (
-    select avg(salary)-500 from employee) and salary < (select avg(salary)+500 from employee);
-
+select eno, ename, salary ,dno from employee where salary > (
+    select avg(salary)-500 from employee ) and salary < (select avg(salary)+500 from employee );
+select r.dno, e.eno, e.ename, e.salary from employee e, (
+    select dno, round(avg(salary)) 평균급여 from employee group by dno) r where e.dno = r.dno and salary between r.평균급여-500 and r.평균급여+500;
 -- 2. 사원번호가 7788인 사원과 담당 업무가 같은 사원을 표시하시오.(사원이름과 담당업무 출력)
 select ename, job  from employee where job=(select job from employee where eno = 7788);
 
 -- 3. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시하시오.(사원이름과 담당업무 출력)
-select ename, job  from employee where salary > (select salary from employee where eno = 7788);
+select ename, job  from employee where salary > (select salary from employee where eno = 7499);
 
 -- 4. job 별로 평균 급여가 가장 적은 job의 평균 급여를 표시하시오.
 select job, avg(salary) from employee group by job having avg(salary) = (
@@ -21,11 +22,36 @@ select eno, job, salary from employee where salary < all(
     select salary from employee where job='ANALYST') and job <> 'ANALYST';
     
 -- 7. 부하직원이 없는 사원의 이름을 표시하시오.
+select * from employee where eno not in (select distinct manager from employee where manager is not null);
 
+-- 8. 부하직원이 있는 사원의 이름을 표시하시오.
+select * from employee where eno in (select distinct manager from employee where manager is not null);
+
+-- 9. BLAKE와 동일한 부서에 속한 사원의 이름과 입사일을 표시하는 질의를 작성하시오(단 BLAKE는 제외)
+select ename, hiredate from employee where dno = (
+    select dno from employee where ename = 'BLAKE') and not ename = 'BLAKE';
+-- 10. 부서 위치가 BUSAN인 사원의 이름과 부서번호 및 담당 업무를 표시하시오.
+select ename, e.dno, job from employee e, department d , loc l where e.dno=d.dno and d.loc=l.loc and cityname='SEOUL';
+select * from employee where dno = (
+    select dno from department where loc= (
+        select loc from loc where cityname = 'SEOUL'));
 -- 11. KING에게 보고하는 사원의 이름과 급여를 표시하시오.
 select ename, salary from employee where manager = (
     select eno from employee where ENAME = 'KING');
 
--- 12.
+-- 12.평균 급여보다 많은 급여를 받고 이름에 M이 포함된 사원과 같은 부서에서 근무하는 사원의 사원번호, 이름, 급여를 표시하시오.
+select DNO, eno, ename, salary from employee where salary > (
+    select avg(salary) from employee) and DNO IN(select dno from employee where ename LIKE '%M%');
     
+-- 13. 담당업무가 ANALYST 인 사원이 소속된 부서와 동일한 부서의 사원을 표시하시오.
+select ename from employee where dno in (
+    select  dno from employee where job = 'ANALYST');
+    
+-- 14. 부서번호, 부서명, 부서별 인원수를 출력하시오.
+select dno, dname, (
+    select count(dno) from employee where dno = d.dno ) from department d;
+    
+-- 15. 각 근무 지역에 대해 근무지역명, 지역번호, 지역별 사원수, 지역별 모든 사원의 평균 급여를 출력하시오.
+select cityname, d.loc, count(eno), avg(salary) from employee e, department d, loc l 
+    where e.dno = d.dno and d.loc=l.loc group by cityname, d.loc;
     
